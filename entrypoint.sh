c36b09ff5d67398c27804ac8d2caa9eb9b070b1e
Mick Ryan <mick@evilbits.io>
2019-09-14 09:53:44 -0700

    add new param for path

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,4 +10,4 @@ chmod 600 "$SSH_PATH/deploy_key"
 
 
 # Do deployment
-sh -c "rsync $INPUT_SWITCHES -e 'ssh -i $SSH_PATH/deploy_key -o StrictHostKeyChecking=no' $INPUT_EXCLUDES $GITHUB_WORKSPACE/ $INPUT_UPLOAD_PATH"
+sh -c "rsync $INPUT_SWITCHES -e 'ssh -i $SSH_PATH/deploy_key -o StrictHostKeyChecking=no' $INPUT_EXCLUDES $GITHUB_WORKSPACE/$INPUT_PATH $INPUT_UPLOAD_PATH"
