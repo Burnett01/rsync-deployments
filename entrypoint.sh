afc3d5d8d4528ba7ee72debcb45ca35fbb5601ed
Steven Agyekum <burnett@posteo.de>
2019-11-12 23:37:25 +0100

    added INPUT_INCLUDES and INPUT_FILTERS args

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,4 +14,4 @@ echo "$DEPLOY_KEY" > "$SSH_PATH/deploy_key"
 chmod 600 "$SSH_PATH/deploy_key"
 
 # Do deployment
-sh -c "rsync $INPUT_SWITCHES -e 'ssh -i $SSH_PATH/deploy_key -o StrictHostKeyChecking=no $INPUT_RSH' $INPUT_EXCLUDES $GITHUB_WORKSPACE/$INPUT_PATH $INPUT_UPLOAD_PATH"
+sh -c "rsync $INPUT_SWITCHES -e 'ssh -i $SSH_PATH/deploy_key -o StrictHostKeyChecking=no $INPUT_RSH' $INPUT_INCLUDES $INPUT_EXCLUDES $INPUT_FILTERS $GITHUB_WORKSPACE/$INPUT_PATH $INPUT_UPLOAD_PATH"
